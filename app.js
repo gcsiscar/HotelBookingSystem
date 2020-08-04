74d2fa9cc297bc3e34756c68205579bec14a672b
Gilbert Christopher Siscar <60701500+Snow24Prince@users.noreply.github.com>
2020-08-05 00:40:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,4 @@ const keys = require('./config/keys');
 mongoose.connect(keys, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
     console.log('Connected to database');
-    done();
 }).on('error', err => console.log(err))
